85c2ecae014f591c804a04afd0f54da673236942
Monir <moniru005@gmail.com>
2023-11-26 03:25:56 +0600

    create token api and create middleware done

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -26,9 +27,47 @@ async function run() {
     // await client.connect();
 
     const userCollection = client.db('emsDB').collection('users');
+    const demoRequestCollection = client.db('emsDB').collection('demoRequest');
+
+    //jwt API
+    app.post("/jwt", async (req, res) => {
+        const user = req.body;
+        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+          expiresIn: "10h",
+        });
+        res.send({ token });
+      });
+
+    //middleware
+    const verifyToken = (req, res, next) =>{
+        console.log('inside verify token', req.headers.authorization);
+        if(!req.headers.authorization){
+            return res.status(401).send({message: 'Unauthorized Access'});
+        }
+        const token = req.headers.authorization.split(" ")[1];
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) =>{
+            if(err){
+                return res.status(401).send({message: 'Unauthorized Access'});
+            }
+            req.decoded = decoded;
+            next();
+        })
+    }
+
+    //Verify Admin
+    const  verifyAdmin = async(req, res, next) =>{
+        const email = req.decoded.email;
+        const query = {email: email};
+        const user = await userCollection.findOne(query);
+        if (!isAdmin) {
+            return res.status(403).send({message: 'Forbidden Access'})
+        }
+        next();
+    }
+
 
     //Users API 
-    app.get('/users', async(req, res) => {
+    app.get('/users', verifyToken, async(req, res) => {
         const result = await userCollection.find().toArray();
         res.send(result);
     })
@@ -41,6 +80,22 @@ async function run() {
             return res.send({message: "User already exists", insertedId: null});
         }
         const result = await userCollection.insertOne(user);
+        res.send(result);
+    })
+
+    app.get('/demo-request', async(req, res) => {
+        const result = await demoRequestCollection.find().toArray();
+        res.send(result);
+    })
+
+    app.post('/demo-request', async(req, res) =>{
+        const user = req.body;
+        const query = {email: user.email};
+        const existingUser = await demoRequestCollection.findOne(query);
+        if(existingUser){
+            return res.send({message: "Email already exists", insertedId: null});
+        }
+        const result = await demoRequestCollection.insertOne(user);
         res.send(result);
     })
 
